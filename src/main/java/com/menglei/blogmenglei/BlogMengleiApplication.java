c123c8003d163e6df282dc09513491f9305ec99e
Yshinu <yshinu2833@gmail.com>
2024-07-16 17:14:11 +0800

    init

---
+++ src/main/java/com/menglei/blogmenglei/BlogMengleiApplication.java
@@ -0,0 +1,13 @@
+package com.menglei.blogmenglei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMengleiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMengleiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/menglei/blogmenglei/BlogMengleiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menglei.blogmenglei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMengleiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
